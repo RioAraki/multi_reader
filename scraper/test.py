6b366864b158bcfd4dd7422f7a396178e6ff2c12
RioAraki <yueee.li@mail.utoronto.ca>
2018-03-05 17:43:16 -0500

    a bit progression on log system

--- scraper/test.py
+++ scraper/test.py
@@ -1,5 +1,12 @@
+import logging
+
 def install():
     print ("In install")
+    logging.basicConfig(filename='example.log', level=logging.DEBUG)  # will create a file to record all logging info
+    logging.debug('This message should go to the log file')
+    logging.info('So should this')
+    logging.warning('And this, too')
+
 
 methods = {'install': install}
 
