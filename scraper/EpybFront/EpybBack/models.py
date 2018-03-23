d0ea9259412a8dbe8c9378a171fbe2a0fd6570d3
RioAraki <yueee.li@mail.utoronto.ca>
2018-03-23 18:30:35 -0400

    finish to build a basic front/backend for epyb using django

--- scraper/EpybFront/EpybBack/models.py
+++ scraper/EpybFront/EpybBack/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here. a blue print for your database
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    count = models.IntegerField()
--- scraper/EpybFront/EpybBack/views.py
+++ scraper/EpybFront/EpybBack/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here. python function
+def index(request):
+    template = loader.get_template('index.html')
+    book_name = ''
+    if request.method == "POST":
+        book_name = request.POST.get("book_name")
+        print (book_name)
+    context = {'return_value': book_name}
+
+    return HttpResponse(template.render(context, request))
